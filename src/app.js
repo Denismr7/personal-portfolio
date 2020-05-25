deb1a6f38eea51a00b4475157e0b53ef15971003
Denis Mory <denismr7@gmail.com>
2020-05-25 21:51:08 +0200

    scroll on mobile fixed

--- src/app.js
+++ src/app.js
@@ -9,9 +9,10 @@ tl.fromTo("#about-text", 1, {x: -200, opacity: 0}, {x: 0, opacity: 1, ease: abou
 tl2.fromTo("#tec-items-container", 1.5, {y: 200, opacity: 0}, {y: 0, opacity: 1, ease: aboutAnimation });
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: "#about-cont-container"
+    triggerElement: "#about-cont-container",
+    triggerHook: 100
 }).setTween(tl).addTo(controller);
 
 const scene2 = new ScrollMagic.Scene({
     triggerElement: ".title-container"
-}).setTween(t2).addTo(controller);
+}).setTween(tl2).addTo(controller);
