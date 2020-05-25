486a33821487a31bdd8df7f085daea68131e2be8
Denis Mory <denismr7@gmail.com>
2020-05-25 15:10:18 +0200

    started contact redesign

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const aboutAnimation = Power2.easeInOut;
 
 tl.fromTo("#personal-photo", 1, {x: 400, opacity: 0}, {x: 0, opacity: 1, ease: aboutAnimation });
 tl.fromTo("#about-text", 1, {x: -400, opacity: 0}, {x: 0, opacity: 1, ease: aboutAnimation }, "=-1");
-tl.fromTo("#tec-items-container", 1, {y: 200, opacity: 0}, {y: 0, opacity: 1, ease: aboutAnimation });
+tl.fromTo("#tec-items-container", 1.5, {y: 200, opacity: 0}, {y: 0, opacity: 1, ease: aboutAnimation });
 
 const scene = new ScrollMagic.Scene({
     triggerElement: "#about h1"
