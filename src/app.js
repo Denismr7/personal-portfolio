cc980c6e8b30757c9c0f2feb36597029f848853c
Denis Mory <denismr7@gmail.com>
2020-05-31 17:45:22 +0200

    mobile query fixed

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ var tl = new TimelineMax();
 var tl2 = new TimelineMax();
 
 const controller = new ScrollMagic.Controller();
-const aboutAnimation = Power2.easeInOut;
+const animation = Power2.easeInOut;
 
-tl.fromTo("#personal-photo", 1, {x: 200, opacity: 0}, {x: 0, opacity: 1, ease: aboutAnimation });
-tl.fromTo("#about-text", 1, {x: -200, opacity: 0}, {x: 0, opacity: 1, ease: aboutAnimation }, "=-1");
-tl2.fromTo("#tec-items-container", 1.5, {y: 200, opacity: 0}, {y: 0, opacity: 1, ease: aboutAnimation });
+tl.fromTo("#personal-photo", 1, {x: 200, opacity: 0}, {x: 0, opacity: 1, ease: animation });
+tl.fromTo("#about-text", 1, {x: -200, opacity: 0}, {x: 0, opacity: 1, ease: animation }, "=-1");
+tl2.fromTo("#tec-items-container", 1, {y: 200, opacity: 0}, {y: 0, opacity: 1, ease: animation });
 
 const scene = new ScrollMagic.Scene({
     triggerElement: "#about-cont-container",
@@ -14,6 +14,6 @@ const scene = new ScrollMagic.Scene({
 }).setTween(tl).addTo(controller);
 
 const scene2 = new ScrollMagic.Scene({
-    triggerElement: ".title-container",
+    triggerElement: "#tec-items-container",
     triggerHook: 100
 }).setTween(tl2).addTo(controller);
